f4a1b9d8373fa763a2c776d3be42d477782d4ed1
Sodara Sou <sodarasou168@gmail.com>
2024-04-26 15:45:04 +0700

    Add .env file with NEXT_PUBLIC_BACKEND_URL configuration

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,123 +1,190 @@
-'use client'
+"use client";
 
-import Button from '@/components/Button'
-import Input from '@/components/Input'
-import InputError from '@/components/InputError'
-import Label from '@/components/Label'
-import Link from 'next/link'
-import { useAuth } from '@/hooks/auth'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import AuthSessionStatus from '@/app/(auth)/AuthSessionStatus'
+// import Input from "@/components/Input";
+// import InputError from "@/components/InputError";
+// import Label from "@/components/Label";
+import Link from "next/link";
+import Image from "next/image";
+import { useAuth } from "@/hooks/auth";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import InputGroup from "@/components/InputGroup";
+import Button from "@/components/Button";
+import styles from "./signIn.module.css";
+import axios from "@/lib/axios";
+// import AuthSessionStatus from "@/app/(auth)/AuthSessionStatus";
 
 const Login = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    const { login } = useAuth({
-        middleware: 'guest',
-        redirectIfAuthenticated: '/dashboard',
-    })
+  const { login } = useAuth({
+    middleware: "guest",
+    redirectIfAuthenticated: "/dashboard",
+  });
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [shouldRemember, setShouldRemember] = useState(false)
-    const [errors, setErrors] = useState([])
-    const [status, setStatus] = useState(null)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [shouldRemember, setShouldRemember] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const [status, setStatus] = useState(null);
 
-    useEffect(() => {
-        if (router.reset?.length > 0 && errors.length === 0) {
-            setStatus(atob(router.reset))
-        } else {
-            setStatus(null)
-        }
-    })
-
-    const submitForm = async event => {
-        event.preventDefault()
-
-        login({
-            email,
-            password,
-            remember: shouldRemember,
-            setErrors,
-            setStatus,
-        })
+  useEffect(() => {
+    if (router.reset?.length > 0 && errors.length === 0) {
+      setStatus(atob(router.reset));
+    } else {
+      setStatus(null);
     }
+  });
 
-    return (
-        <>
-            <AuthSessionStatus className="mb-4" status={status} />
-            <form onSubmit={submitForm}>
-                {/* Email Address */}
-                <div>
-                    <Label htmlFor="email">Email</Label>
+  const [inputData, setInputData] = useState({
+    email: "",
+    password: "",
+  });
+  const onChange = (e) => {
+    e.preventDefault();
+    setInputData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
-                    <Input
-                        id="email"
-                        type="email"
-                        value={email}
-                        className="block mt-1 w-full"
-                        onChange={event => setEmail(event.target.value)}
-                        required
-                        autoFocus
-                    />
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    login({
+      email,
+      password,
+      remember: shouldRemember,
+      setErrors,
+      setStatus,
+    });
+  };
+  const handleGoogleLogin = async (e) => {
+    e.preventDefault();
+    // await axios.get("/sanctum/csrf-cookie");
+    router.push("http://127.0.0.1:8000/auth/google/redirect");
+  };
+  return (
+    <>
+      <section className="h-screen flex flex-col justify-center items-center p-4 xl:p-0">
+        <div className="w-full md:max-w-5xl flex">
+          <div
+            className={`hidden w-full md:w-1/2 md:block ${styles.imgContainer}`}
+          >
+            <Image
+              src="/logo_latin.png"
+              alt="logo"
+              fill
+              className={styles.img}
+            />
+          </div>
+          <form
+            onSubmit={onSubmit}
+            className="w-full md:w-1/2 p-10 bg-white border border-gray-200 shadow rounded-xl"
+          >
+            <h1 className="text-4xl font-bold mb-10">Sign In</h1>
+            <div className="flex flex-col gap-4">
+              <InputGroup
+                title="Phone Number"
+                id="phone"
+                type="text"
+                onChange={onChange}
+                placeholder="Enter Phone Number"
+                isRequired={true}
+              />
+              <InputGroup
+                title="Password"
+                id="password"
+                type="password"
+                onChange={onChange}
+                placeholder="********"
+                isRequired={true}
+              />
+              <Button type="submit">Sign In</Button>
+              <div className="flex justify-center mt-6">
+                <p>
+                  Don't have Account?{" "}
+                  <Link href="sign-up" className="hover:underline">
+                    Sign Up
+                  </Link>
+                </p>
+              </div>
+              <div className="flex w-full items-center justify-center gap-2">
+                <div className="w-[140px] border-b-[1px] border-[#d9d9d9]"></div>
+                <h1>Sign In With</h1>
+                <div className="w-[140px] border-b-[1px] border-[#d9d9d9]"></div>
+              </div>
+              <div className="flex w-full justify-center gap-10">
+                <a href="#">
+                  <img alt="fb" className="h-[40px] w-[40px]" />
+                </a>
+                {/* <a href="https://api.tarang.site/auth/google/redirect">
+                  <img alt="google" className="h-[40px] w-[40px]" />
+                </a> */}
+                <Button onClick={handleGoogleLogin}>Google</Button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+      {/* <AuthSessionStatus className="mb-4" status={status} />
+      <form onSubmit={submitForm}>
+        <div>
+          <Label htmlFor="email">Email</Label>
 
-                    <InputError messages={errors.email} className="mt-2" />
-                </div>
+          <Input
+            id="email"
+            type="email"
+            value={email}
+            className="block mt-1 w-full"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            autoFocus
+          />
 
-                {/* Password */}
-                <div className="mt-4">
-                    <Label htmlFor="password">Password</Label>
+          <InputError messages={errors.email} className="mt-2" />
+        </div>
+        <div className="mt-4">
+          <Label htmlFor="password">Password</Label>
 
-                    <Input
-                        id="password"
-                        type="password"
-                        value={password}
-                        className="block mt-1 w-full"
-                        onChange={event => setPassword(event.target.value)}
-                        required
-                        autoComplete="current-password"
-                    />
+          <Input
+            id="password"
+            type="password"
+            value={password}
+            className="block mt-1 w-full"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            autoComplete="current-password"
+          />
 
-                    <InputError
-                        messages={errors.password}
-                        className="mt-2"
-                    />
-                </div>
+          <InputError messages={errors.password} className="mt-2" />
+        </div>
+        <div className="block mt-4">
+          <label htmlFor="remember_me" className="inline-flex items-center">
+            <input
+              id="remember_me"
+              type="checkbox"
+              name="remember"
+              className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              onChange={(e) => setShouldRemember(e.target.checked)}
+            />
 
-                {/* Remember Me */}
-                <div className="block mt-4">
-                    <label
-                        htmlFor="remember_me"
-                        className="inline-flex items-center">
-                        <input
-                            id="remember_me"
-                            type="checkbox"
-                            name="remember"
-                            className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                            onChange={event =>
-                                setShouldRemember(event.target.checked)
-                            }
-                        />
+            <span className="ml-2 text-sm text-gray-600">Remember me</span>
+          </label>
+        </div>
 
-                        <span className="ml-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
+        <div className="flex items-center justify-end mt-4">
+          <Link
+            href="/forgot-password"
+            className="underline text-sm text-gray-600 hover:text-gray-900"
+          >
+            Forgot your password?
+          </Link>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href="/forgot-password"
-                        className="underline text-sm text-gray-600 hover:text-gray-900">
-                        Forgot your password?
-                    </Link>
+          <Button className="ml-3">Login</Button>
+        </div>
+      </form> */}
+    </>
+  );
+};
 
-                    <Button className="ml-3">Login</Button>
-                </div>
-            </form>
-        </>
-    )
-}
-
-export default Login
+export default Login;
--- src/components/ReservationTable.jsx
+++ src/components/ReservationTable.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getReservation, deleteReservation } from "@/services/reservation";
 import { MoreHorizontal } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -26,84 +30,110 @@ import {
 } from "@/components/ui/table";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import ReservationForm from "@/components/ReservationForm";
+import Spinner from "./Spinner";
 
 function ReservationTable() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["reservations"],
+    queryFn: async () => await getReservation(),
+  });
   return (
-    <Card className="bg-white rounded-xl">
-      <CardHeader>
-        <div className="flex justify-between">
-          <div>
-            <CardTitle>Reservations</CardTitle>
-            <CardDescription>Manage your Reservations.</CardDescription>
-          </div>
-          <div>
-            <ReservationForm />
-          </div>
+    <>
+      {isLoading ? (
+        <div className="flex justify-center p-10">
+          <Spinner />
         </div>
-      </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="hidden w-[100px] sm:table-cell">
-                <span className="sr-only">Image</span>
-              </TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead className="hidden md:table-cell">Price</TableHead>
-              <TableHead className="hidden md:table-cell">
-                Total Sales
-              </TableHead>
-              <TableHead className="hidden md:table-cell">Created at</TableHead>
-              <TableHead>
-                <span className="sr-only">Actions</span>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="hidden sm:table-cell">
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </TableCell>
-              <TableCell className="font-medium">
-                Laser Lemonade Machine
-              </TableCell>
-              <TableCell>
-                <Badge variant="outline">Draft</Badge>
-              </TableCell>
-              <TableCell className="hidden md:table-cell">$499.99</TableCell>
-              <TableCell className="hidden md:table-cell">25</TableCell>
-              <TableCell className="hidden md:table-cell">
-                2023-07-12 10:42 AM
-              </TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button aria-haspopup="true" size="icon" variant="ghost">
-                      <MoreHorizontal className="h-4 w-4" />
-                      <span className="sr-only">Toggle menu</span>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuItem>Edit</DropdownMenuItem>
-                    <DropdownMenuItem>Delete</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </CardContent>
-      <CardFooter>
-        <div className="text-xs text-muted-foreground">
-          Showing <strong>1-10</strong> of <strong>32</strong> products
-        </div>
-      </CardFooter>
-    </Card>
+      ) : (
+        <Card className="bg-white rounded-xl">
+          <CardHeader>
+            <div className="flex justify-between">
+              <div>
+                <CardTitle>Reservations</CardTitle>
+                <CardDescription>Manage your Reservations.</CardDescription>
+              </div>
+              <div>
+                <ReservationForm />
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="hidden w-[100px] sm:table-cell">
+                    <span className="sr-only">Image</span>
+                  </TableHead>
+                  <TableHead>Phone Number</TableHead>
+                  <TableHead>Sport</TableHead>
+                  <TableHead className="hidden md:table-cell">
+                    Venue ID
+                  </TableHead>
+                  <TableHead className="hidden md:table-cell">Time</TableHead>
+                  <TableHead className="hidden md:table-cell">Date</TableHead>
+                  <TableHead>
+                    <span className="sr-only">Actions</span>
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {data?.data.reservations.map((reservation, index) => (
+                  <TableRow key={index}>
+                    <TableCell className="hidden sm:table-cell">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                    </TableCell>
+                    <TableCell className="font-medium">
+                      {reservation.phone}
+                    </TableCell>
+                    <TableCell>
+                      <Badge variant="outline">
+                        {reservation.venue.sportTypes.name}
+                      </Badge>
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {reservation.venue.id}
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {reservation.start_time} - {reservation.end_time}
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {reservation.date}
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem>Edit</DropdownMenuItem>
+                          <DropdownMenuItem>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+          <CardFooter>
+            <div className="text-xs text-muted-foreground">
+              Showing <strong>1-10</strong> of{" "}
+              <strong>{data?.data.reservations.length}</strong> reservations
+            </div>
+          </CardFooter>
+        </Card>
+      )}
+    </>
   );
 }
 
