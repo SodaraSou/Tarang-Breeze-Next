eba25991425d0171cc5fccc7b0ee02bc2b342c8a
Sodara Sou <121216473+SodaraSou@users.noreply.github.com>
2024-05-08 17:48:21 +0700

    Remove refetch interval

--- src/lib/Providers/ReactQueryProvider.js
+++ src/lib/Providers/ReactQueryProvider.js
@@ -7,12 +7,12 @@ const ReactQueryProvider = ({ children }) => {
   const [queryClient] = useState(
     () =>
       new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 6 * 1000,
-            refetchInterval: 3 * 1000,
-          },
-        },
+        // defaultOptions: {
+        //   queries: {
+        //     staleTime: 6 * 1000,
+        //     refetchInterval: 3 * 1000,
+        //   },
+        // },
       })
   );
   return (
