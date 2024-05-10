4aa2a6adc66354f1642d03a90004e13db6d3c262
Sodara Sou <121216473+SodaraSou@users.noreply.github.com>
2024-05-10 22:30:37 +0700

    Converted to reservation card

--- src/components/TournamentCard.jsx
+++ src/components/TournamentCard.jsx
@@ -1,66 +1,55 @@
 "use client";
 
-import { Button } from "./ui/button";
-import { MapPin, Clock, Users } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
+import { format } from "date-fns";
+import { MapPin, Clock, Users, CheckCircle } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
-function TournamentCard() {
+function TournamentCard({ reservation }) {
   return (
     <Card className="bg-white rounded-xl">
       <CardHeader>
         <div className="flex flex-row items-center justify-between">
-          <CardTitle className="text-xl">Name of the Tournament</CardTitle>
+          <CardTitle className="text-xl flex items-center gap-2">
+            <MapPin className="w-4 h-4" /> {reservation.venue.name}
+          </CardTitle>
           <div className="flex flex-col">
             <span className="text-center px-4 bg-red-500 rounded-t-xl text-white">
-              SEP
+              {format(reservation.date, "MMMM")}
             </span>
             <span className="text-center px-4 bg-[#eaeaea] rounded-b-xl">
-              30
+              {format(reservation.date, "dd")}
             </span>
           </div>
         </div>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col gap-4">
-          <p className="flex items-center gap-2">
-            <MapPin className="w-4 h-4" />
-            Place of the tournament
-          </p>
-          <p className="flex items-center gap-2">
-            <Clock className="w-4 h-4" />
-            Time
-          </p>
-          <p className="flex items-center gap-2">
-            <Users className="w-4 h-4" />
-            Amount of people joined
-          </p>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="w-full md:w-1/2 flex flex-col gap-4">
+            <p className="flex items-center gap-2">
+              <Clock className="w-4 h-4" />
+              Time: {reservation.start_time} - {reservation.end_time}
+            </p>
+            <p className="flex items-center gap-2">
+              <Users className="w-4 h-4" />
+              Player: {reservation.attendee}
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 flex flex-col gap-4 text-left">
+            {reservation.find_team === 1 && (
+              <p className="flex items-center gap-2">
+                <CheckCircle className="w-4 h-4" />
+                Available for a match
+              </p>
+            )}
+            {reservation.find_member === 1 && (
+              <p className="flex items-center gap-2">
+                <CheckCircle className="w-4 h-4" />
+                Accepting new members
+              </p>
+            )}
+          </div>
         </div>
       </CardContent>
-      <CardFooter>
-        <div className="flex w-full gap-4">
-          <Button
-            variant="outline"
-            className="bg-[#2AD5A5] w-full rounded-xl text-white"
-            onClick={() => {
-              console.log("Button clicked!");
-            }}
-          >
-            Join
-          </Button>
-          <Button
-            variant="outline"
-            className="bg-[#2AD5A5] w-full rounded-xl text-white"
-          >
-            Challenge
-          </Button>
-        </div>
-      </CardFooter>
     </Card>
   );
 }
