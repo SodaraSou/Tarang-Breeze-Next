bcbb126445d1f5148e394f3fd7ddb392960817a3
Sodara Sou <121216473+SodaraSou@users.noreply.github.com>
2024-05-13 22:20:53 +0700

    Done fixed loading

--- src/components/SportDeleteDailog.jsx
+++ src/components/SportDeleteDailog.jsx
@@ -14,40 +14,75 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { deleteSport } from "@/services/sport";
+import Spinner from "@/components/Spinner";
 
-function SportDeleteDailog({sport}) {
-    const [open, setOpen] = useState(false);
-    const handleDelete = async () => {
-      const res = await deleteSport(sport);
-      if (res.status === 204) {
-        setOpen(false);
-        alert("Delete Successful");
-      }
-    };
-    return (
-      <AlertDialog open={open} onOpenChange={setOpen}>
-        <AlertDialogTrigger asChild>
-          <Button variant="outline" className="bg-red-500 text-white">
-            Delete 
-          </Button>
-        </AlertDialogTrigger>
+const wait = () => new Promise((resolve) => setTimeout(resolve, 5000));
+
+function SportDeleteDailog({ sport }) {
+  const [loading, setLoading] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [open, setOpen] = useState(false);
+  const [openAlertDialog, setOpenAlertDialog] = useState(false);
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const res = await deleteSport(sport);
+    if (res.status === 204) {
+      setOpenAlertDialog(true);
+      setAlertMessage("Sport Delete Successfully");
+      wait().then(() => setOpenAlertDialog(false));
+    } else {
+      setOpenAlertDialog(true);
+      setAlertMessage("Sport Delete Failed");
+      wait().then(() => setOpenAlertDialog(false));
+    }
+    setOpen(false);
+    setLoading(false);
+  };
+  return (
+    <>
+      <AlertDialog open={openAlertDialog} onOpenChange={setOpenAlertDialog}>
         <AlertDialogContent>
           <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete{" "} 
-              <span className="font-bold">sport ID {sport.id} </span>
-            </AlertDialogDescription>
+            <AlertDialogTitle>{alertMessage}</AlertDialogTitle>
           </AlertDialogHeader>
           <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction asChild>
-              <Button onClick={handleDelete}>Continue</Button>
-            </AlertDialogAction>
+            <AlertDialogAction>Ok</AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-    );
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline" className="bg-red-500 text-white">
+            Delete
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          {loading ? (
+            <div className="flex justify-center p-10">
+              <Spinner />
+            </div>
+          ) : (
+            <>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete{" "}
+                  <span className="font-bold">sport ID {sport.id} </span>
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction asChild>
+                  <Button onClick={handleDelete}>Continue</Button>
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </>
+          )}
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
 
-export default SportDeleteDailog
+export default SportDeleteDailog;
