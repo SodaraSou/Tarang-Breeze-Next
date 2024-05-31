72d6d07a3f7a07bbe3604da9892a4e885e5d2100
Sodara Sou <121216473+SodaraSou@users.noreply.github.com>
2024-05-31 22:31:52 +0700

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,27 +15,38 @@ function Header() {
   return (
     <header className="w-full bg-white">
       <div className="p-4 flex justify-between items-center">
-        <Link href="/">
-          <Image src="/tarang_logo.png" alt="logo" width={150} height={50} />
-        </Link>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <span className="sr-only">Toggle user menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Link href="/profile">Profile</Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="md:w-1/4">
+          <Link href="/">
+            <Image src="/tarang_logo.png" alt="logo" width={150} height={50} />
+          </Link>
+        </div>
+        <nav className="hidden xl:flex justify-center gap-10">
+          <Link href="/sport/football">Football</Link>
+          <Link href="/sport/badminton">Badminton</Link>
+          <Link href="/sport/volleyball">Volleyball</Link>
+          <Link href="/sport/ping%20pong">Ping Pong</Link>
+          <Link href="/about-us">About Us</Link>
+        </nav>
+        <div className="md:w-1/4 flex justify-end">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link href="/profile">Profile</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </header>
   );
