db840c3adc44ac311091656b7cba954490edab38
Sodara Sou <121216473+SodaraSou@users.noreply.github.com>
2024-06-11 22:46:55 +0700

    update policy

--- src/app/policy/page.jsx
+++ src/app/policy/page.jsx
@@ -1,88 +1,109 @@
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
 import UserLayout from "@/app/UserLayout";
-import React from "react";
 
 function PolicyPage() {
   return (
     <UserLayout>
-      <div className="max-w-7xl mx-auto my-16">
-        <h1 className="text-3xl font-bold text-center">Tarang Policy</h1>
-        <p className="mt-4 mb-16">
-          Welcome to Tarang, your trusted platform for booking sports venues. We
-          are dedicated to making sports accessible, enjoyable, and inclusive
-          for everyone. Please read our booking policy carefully to ensure a
-          smooth and enjoyable experience.
-        </p>
-        <div className="bg-gray-200 h-0.5"></div>
-        <div className="flex flex-col gap-10 my-16">
-          <div>
-            <h1 className="text-2xl font-bold mb-2">Booking Guidelines</h1>
+      <div className="p-4 md:p-10">
+        <Card className="max-w-5xl mx-auto bg-white">
+          <CardHeader>
+            <h1 className="text-3xl font-bold text-center mb-2">
+              Tarang Policy
+            </h1>
             <p>
-              <b>Advance Booking Requirement:</b> To ensure the availability of
-              venues and adequate preparation time, all bookings must be made at
-              least two days before the intended game day. This allows us to
-              confirm your reservation and ensure that the venue meets your
-              needs.
+              Welcome to Tarang, your trusted platform for reservation sports
+              venues. We are dedicated to making sports accessible, enjoyable,
+              and inclusive for everyone. Please read our reservation policy
+              carefully to ensure a smooth and enjoyable experience.
             </p>
-            <p>
-              <b>Venue Availability:</b> Our platform provides real-time updates
-              on venue availability. Once your booking is confirmed, you will
-              receive a confirmation email with all the necessary details.
-            </p>
-            <p>
-              <b>Booking Confirmation:</b> A booking is only confirmed once you
-              receive a confirmation email from Tarang. Please ensure all
-              details are accurate and inform us immediately if any changes are
-              required.
-            </p>
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold mb-2">Team Matching Service</h1>
-            <p>
-              <b>Find a Team to Play Against:</b> If you're looking for a team
-              to compete against, Tarang offers a team matching service. Simply
-              let us know your preferences and skill level, and we will help
-              connect you with an appropriate team.
-            </p>
-            <p>
-              <b>Team Availability:</b> While we strive to find the best
-              matches, team availability is subject to scheduling and demand. We
-              recommend booking early and providing detailed preferences to
-              increase the chances of a good match.
-            </p>
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold mb-2">Code of Conduct</h1>
-            <p>
-              <b>Respect and Fair Play:</b> All players and teams are expected
-              to adhere to principles of respect and fair play. Any form of
-              discrimination, harassment, or unsportsmanlike conduct will not be
-              tolerated and may result in a ban from using our services.
-            </p>
-            <p>
-              <b>Venue Rules:</b> Please adhere to the rules and regulations of
-              the booked venue. Failure to comply with venue rules may result in
-              penalties or additional charges.
-            </p>
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold mb-2">Changes to Policy</h1>
-            <p>
-              Tarang reserves the right to update or modify this booking policy
-              at any time. Any changes will be communicated to users via email
-              and updated on our website. Please review this policy periodically
-              to stay informed of any changes.
-            </p>
-          </div>
-          <div className="mb-16">
-            <h1 className="text-2xl font-bold mb-2">Contact Us</h1>
-            <p>
-              If you have any questions or need assistance, please do not
-              hesitate to contact our support team at support@tarang.com or call
-              us at (123) 456-7890. We are here to help you with any queries or
-              concerns.
-            </p>
-          </div>
-        </div>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-6">
+              <div>
+                <h1 className="text-2xl font-bold mb-2">
+                  Reservation Guidelines
+                </h1>
+                <p>
+                  <b>Advance Reservation Requirement:</b> To ensure the
+                  availability of venues and adequate preparation time, all
+                  reservations must be made at least 48 hours (2 days) before
+                  the intended game day.
+                </p>
+                <p>
+                  <b>Change Reservation Requirement:</b> Reservations can be
+                  changed at least 24 hours (1 day) before the intended game
+                  day. You need to contact the Tarang for any change to your
+                  reservation.
+                </p>
+                <p>
+                  <b>Cancel Reservation Requirement:</b> Reservations can be
+                  canceled at least 24 hours (1 day) notice before the intended
+                  game day.
+                </p>
+                <p>
+                  <b>On Call Reservation:</b> You can also reserved venue via
+                  Tarang Social or Phone Call. Your reservation will be
+                  confirmed via SMS.
+                </p>
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold mb-2">
+                  Match Hosting Guidelines
+                </h1>
+                <p>
+                  <b>For The Host:</b> You can create your match game when you
+                  reserved the venue, but for on call reservation we will be
+                  unable to host a match game for you. You can reject or accept
+                  your opponent or cancel your match game in the Match Game
+                  panel in your Profile. Any change to the match game you have
+                  to notify Tarang atleast 24 hours (1 day) before the intended
+                  game day.
+                </p>
+                <p>
+                  <b>For The Challenger:</b> You can accept or withdraw your
+                  match game in the Match Game panel in your Profile. Any change
+                  to the match game you have to notify the Host atleast 24 hours
+                  (1 day) before the intended game day. Tarang will not
+                  responsible for any altercation between the Host and the
+                  Challenger.
+                </p>
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold mb-2">Code of Conduct</h1>
+                <p>
+                  <b>Respect and Fair Play:</b> All players and teams are
+                  expected to adhere to principles of respect and fair play. Any
+                  form of discrimination, harassment, or unsportsmanlike conduct
+                  will not be tolerated and may result in a ban from using our
+                  services.
+                </p>
+                <p>
+                  <b>Venue Rules:</b> Please adhere to the rules and regulations
+                  of the reserved venue. Failure to comply with venue rules may
+                  result in penalties or additional charges.
+                </p>
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold mb-2">Changes to Policy</h1>
+                <p>
+                  Tarang reserves the right to update or modify this reservation
+                  policy at any time. Any changes will be communicated to users
+                  via our social and updated on our website. Please review this
+                  policy periodically to stay informed of any changes.
+                </p>
+              </div>
+              <div className="mb-16">
+                <h1 className="text-2xl font-bold mb-2">Contact Us</h1>
+                <p>
+                  If you have any questions or need assistance, please do not
+                  hesitate to contact our support team at support@tarang.site or
+                  call us at (123) 456-7890. We are here to help you with any
+                  queries or concerns.
+                </p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </UserLayout>
   );
